9f95464f7a7e6db158035d0ff4c46e0ada629c50
wangwz2012@sina.cn <wangwz2012@sina.cn>
2024-06-16 22:22:19 +0800

    update

--- gallery/mobile/images.py
+++ gallery/mobile/images.py
@@ -4,7 +4,7 @@ path = os.path.split(os.path.realpath(__file__))[0]
 
 path2 = path[path.rfind("source")+6:]
 
-os.chdir(path+"/images/microsoft")
+os.chdir(path+"/images/")
 ls_file = []
 for file in os.scandir():
     if file.is_file():
@@ -13,7 +13,7 @@ for file in os.scandir():
 md_text = "\n{% gallery %}\n"
 
 for image in ls_file:
-    md_path = "!["+os.path.splitext(image)[0]+"]("+str(path2)+"/images/microsoft/"+image+")"
+    md_path = "!["+os.path.splitext(image)[0]+"]("+str(path2)+"/images/"+image+")"
     md_text += md_path.replace("\\", "/")+"\n"
 
 md_text += "{% endgallery %}"
